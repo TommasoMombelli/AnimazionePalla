d593409376d22d8fb28d91be7a028a6c28d8b56e
Andrea Sbarra <a.sbarra@studenti.unibs.it>
2024-10-09 15:43:12 +0200

    Ho spostato la funzione animiazione palla direttamente nel server

--- client.py
+++ client.py
@@ -12,9 +12,9 @@ def client_program():
 
     while message.lower().strip() != 'bye': 
         client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+        # data = client_socket.recv(1024).decode()  # receive response
 
-        print('Received from server: ' + data)  # show in terminal
+        # print('Received from server: ' + data)  # show in terminal
 
         message = input(" -> ")  # again take input
 

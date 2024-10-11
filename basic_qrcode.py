da54dfc6dbf279be646d9db9732c930ca7124c77
Ilaria342 <i.reghenzi@studenti.unibs.it>
2024-10-11 17:31:14 +0200

    funzione per generare il qr code

---
+++ basic_qrcode.py
@@ -0,0 +1,27 @@
+import socket
+from tkinter import Image
+import segno
+import io
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+
+def qr_code():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+
+    qrcode = segno.make_qr(IPAddr)
+    buffer = io.BytesIO()
+    qrcode.save(buffer, kind='png', scale=13)
+    buffer.seek(0)
+
+    img = Image.open(buffer)
+    plt.imshow(img)
+    plt.axis('off')  # Hide axes
+    plt.show()  
+
+if __name__ == '__main__':
+    qr_code()
+
+
